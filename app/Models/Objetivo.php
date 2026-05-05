209d6adee7e95abff244a3ab9b281e7ace24b31f
Aaron <zertucheaaron9@gmail.com>
2026-05-05 12:11:13 -0600

    creacion de las migraciones y el proyecto

---
+++ app/Models/Objetivo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Objetivo extends Model
+{
+    /** @use HasFactory<\Database\Factories\ObjetivoFactory> */
+    use HasFactory;
+}
---
+++ database/factories/ObjetivoFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Objetivo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Objetivo>
+ */
+class ObjetivoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
